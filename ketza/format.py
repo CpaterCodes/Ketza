a5a92209199dd04881cadc86f157adf7c0ebd8a3
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2022-12-20 15:27:50 +0000

    Feat: Can now indent raw html output from Ketza. Can also simply add linebreaks.

--- ketza/format.py
+++ ketza/format.py
@@ -1,15 +1,55 @@
 from typing import Literal
+from re import findall
 
 def indent(
         raw_html: str, 
         indenter: Literal['tabs', 'spaces'] = 'tabs'
 ):
+
+    # Mark out linebreaks
+    raw_html = linebreaks(raw_html)
+
+    # Add indents, starting by getting full rows
+    rows = raw_html.split('\n')
+    indented_rows = []
+    depth: int = 0
     tab = '\t' if indenter == 'tabs' else "    "
+    for row in rows:
+        if _is_closing_tag(row):
+            depth -= 1
+            indented_rows.append(f"{tab * depth}{row}\n")
 
-    # Add linebreaks
-    raw_html = raw_html.replace('>', ">\n")
+        elif _is_opening_tag(row):
+            indented_rows.append(f"{tab * depth}{row}\n")
+            depth += 1
 
-   # Add indents
-   # TODO...
-    return ''.join(raw_html)
+        else:
+            indented_rows.append(f"{tab * depth}{row}\n")
 
+
+    return ''.join(indented_rows[:-1])
+
+def linebreaks(raw_html: str):
+    return raw_html.replace(
+            '>', ">\n"
+        ).replace(
+            '<', "\n<"
+        ).replace(
+            "\n\n", "\n"
+            )[1:]
+
+
+def _is_closing_tag(row: str): 
+    return "</" in row
+
+
+def _is_opening_tag(row: str):
+    return _is_tag(row) and not _is_oneline_tag(row)
+
+
+def _is_oneline_tag(row: str):
+    return "/>" in row
+
+
+def _is_tag(row: str):
+    return "<" in row and ">" in row
--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -1,5 +1,14 @@
 import pytest
-from ..ketza.format import indent
+from ..ketza.format import indent, linebreaks
+
+def test_linebreaks():
+    linebroken_html: str
+    with open("tests/linebreaks_sample.html", "r") as f:
+        linebroken_html = f.read()
+
+    unbroken_html = '<body id="App"><p>Hello</p><p>World</p></body>'
+    assert linebreaks(unbroken_html) == linebroken_html
+
 
 def test_indent():
     indented_html: str
