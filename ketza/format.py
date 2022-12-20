425416d80bced270e6d4e09cee18c19baeeeec26
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2022-12-20 00:05:33 +0000

    Tweak: Adjustment to sample html. Indent function adds linebreaks. May be most elegant to employ regex for isolating opening/closing tags and applying indents

--- ketza/format.py
+++ ketza/format.py
@@ -1,5 +1,15 @@
+from typing import Literal
 
-def indent(unindented_html: str):
-    chars = list(unindented_html)
-    return chars
+def indent(
+        raw_html: str, 
+        indenter: Literal['tabs', 'spaces'] = 'tabs'
+):
+    tab = '\t' if indenter == 'tabs' else "    "
+
+    # Add linebreaks
+    raw_html = raw_html.replace('>', ">\n")
+
+   # Add indents
+   # TODO...
+    return ''.join(raw_html)
 
