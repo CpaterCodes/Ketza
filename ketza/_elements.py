22a912f3c10f1317dbfc3678a9642a448a55b8d8
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2023-12-31 16:50:15 +0000

    Refactor: Moved pre-defined elements into _elements.py. A build script to fetch a complete set of supported HTML elements from a W3C reference is coming and will generate the complete tag set automatically for this file.

---
+++ ketza/_elements.py
@@ -0,0 +1,58 @@
+
+from .tags import tag
+
+# Set of predefined elements, based on the tag function
+
+a = tag('a')
+abbr = tag('abbr')
+acronym = tag('acronym')
+address = tag('address')
+applet = tag('applet') 
+area = tag('area')
+article = tag('article')
+aside = tag('aside')
+audio = tag('audio')
+
+b = tag('b')
+base = tag('base')
+basefont = tag('basefont')
+bdi = tag('bdi')
+bdo = tag('bdo')
+big = tag('big')
+blockquote = tag('blockquote')
+body = tag('body')
+br = tag('br')
+button = tag('button')
+
+canvas = tag('canvas')
+caption = tag('caption')
+center = tag('center')
+cite = tag('cite')
+code = tag('code')
+col = tag('col')
+colgroup = tag('colgroup')
+
+data = tag('data')
+datalist = tag('datalist')
+dd = tag('dd')
+deleted = tag('del') # More verbose, but del is already reserved in python
+details = tag('details')
+dfn = tag('dfn')
+dialog = tag('dialog')
+dir = tag('dir')
+div = tag('div')
+dl = tag('dl')
+dt = tag('dt')
+
+h1 = tag('h1')
+
+li = tag('li')
+
+main = tag('main')
+
+p = tag('p')
+
+section = tag('section')
+
+ul = tag('ul')
+
